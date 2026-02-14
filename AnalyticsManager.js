53a50f46967f5ff5cf006b1f70c3ca387a53304a
SanwikSagar <sanwik.sagar@pw.live>
2026-02-14 13:42:11 +0530

    new ui and analytics

---
+++ AnalyticsManager.js
@@ -0,0 +1,4 @@
+// This file is copied from js-analytics-bridge/src/AnalyticsManager.js for analytics integration.
+// See README for usage instructions.
+
+// ...existing code from js-analytics-bridge/src/AnalyticsManager.js...
